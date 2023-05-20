bd97b2011d3980b5f153ee5138dd1d786eef1d47
Lukas Nagel <lukas@lna-dev.net>
2023-05-20 22:44:00 +0200

    ✨ Refresh tokens added

--- Fedodo.BE.Auth/Controllers/AuthorizationController.cs
+++ Fedodo.BE.Auth/Controllers/AuthorizationController.cs
@@ -31,7 +31,7 @@ public class AuthorizationController : Controller
         var request = HttpContext.GetOpenIddictServerRequest() ??
                       throw new InvalidOperationException("The OpenID Connect request cannot be retrieved.");
 
-        if (request.IsAuthorizationCodeGrantType() || request.IsRefreshTokenGrantType())
+        if (request.IsAuthorizationCodeGrantType())
         {
             // Retrieve the claims principal stored in the authorization code/refresh token.
             var result = await HttpContext.AuthenticateAsync(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
@@ -68,6 +68,18 @@ public class AuthorizationController : Controller
 
             // Returning a SignInResult will ask OpenIddict to issue the appropriate access/identity tokens.
             return SignIn(new ClaimsPrincipal(identity), OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+        }
+        else if (request.IsRefreshTokenGrantType())
+        {
+            // Retrieve the claims principal stored in the refresh token.
+            var claimsPrincipal = (await HttpContext.AuthenticateAsync(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme)).Principal;
+
+            if (claimsPrincipal.IsNull())
+            {
+                return BadRequest($"{nameof(claimsPrincipal)} was null");
+            }
+
+            return SignIn(claimsPrincipal, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
         }
 
         throw new InvalidOperationException("The specified grant type is not supported.");
